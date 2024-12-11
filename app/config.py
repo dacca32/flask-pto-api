3f298e7ed077410f376aa3894f708cd488b03f94
Scot Darcy <scotdarcymac@gmail.com>
2024-12-11 10:48:25 +0000

    Configured postgres

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     JWT_SECRET_KEY = 'qwertyuioplkjhgfdsazxcvbnm'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///pto.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://scot:password123!@localhost:5432/pto_db'
     JWT_BLACKLIST_TOKEN_CHECKS = ['access', 'refresh']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_BLACKLIST_ENABLED = True
